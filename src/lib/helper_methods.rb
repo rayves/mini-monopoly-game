97c4b75699528cc29bf862862b4918e63b0b1328
rayves <vo.raymond.v@outlook.com>
2022-07-28 21:34:48 +1000

    creates generate players and spaces methods (manually tested)

--- src/lib/helper_methods.rb
+++ src/lib/helper_methods.rb
@@ -2,3 +2,16 @@ def read_json(file_path)
   JSON.parse(File.read(file_path))
 end
 
+def generate_players(player_names)
+  player_names.map { |name| Player.new(name) }
+end
+
+def generate_spaces(board)
+  board.map do |space|
+    if space['type'] != 'property'
+      Space.new(space['name'], space['price'], space['colour'], space['type'], 'Cannot be purchased')
+    else
+      Space.new(space['name'], space['price'], space['colour'], space['type'], 'Unowned')
+    end
+  end
+end
