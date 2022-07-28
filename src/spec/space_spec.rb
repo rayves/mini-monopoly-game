863c601cc93b3cf8ceb07ce412625d5013917303
rayves <vo.raymond.v@outlook.com>
2022-07-28 21:51:48 +1000

    creates tests for Space class - tests Space class creates object instances properly

--- src/spec/space_spec.rb
+++ src/spec/space_spec.rb
@@ -1,1 +1,31 @@
 require_relative '../lib/space'
+
+describe 'Space' do
+  before(:each) do
+    @space = Space.new('The Burvale', 1, 'Brown', 'property', 'unowned')
+  end
+
+  it 'should be an instance of Space' do
+    expect(@space).to be_a Space
+  end
+
+  it 'should have a name attribute' do
+    expect(@space.name).to eq('The Burvale')
+  end
+
+  it 'should have a price' do
+    expect(@space.price).to eq(1)
+  end
+
+  it 'should have a colour' do
+    expect(@space.colour).to eq('Brown')
+  end
+
+  it 'should have a type' do
+    expect(@space.type).to eq('property')
+  end
+
+  it 'should have an ownership classification' do
+    expect(@space.owner).to eq('unowned')
+  end
+end
