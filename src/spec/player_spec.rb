e322e56bdf88ceef6c9c8e57a1ab06df8ca5f8f2
rayves <vo.raymond.v@outlook.com>
2022-07-28 21:55:51 +1000

    creates more basic functionality tests for Player class - tests function properly

--- src/spec/player_spec.rb
+++ src/spec/player_spec.rb
@@ -12,4 +12,16 @@ describe 'Player' do
   it 'should have a name attribute' do
     expect(@player.name).to eq('Ray')
   end
+
+  it 'should start with $16 in its wallet' do
+    expect(@player.wallet).to eq(16)
+  end
+
+  it 'should hold not properties when created' do
+    expect(@player.properties.count).to eq(0)
+  end
+
+  it 'should start at 0 for its current position' do
+    expect(@player.current_position).to eq(0)
+  end
 end
