4d6648f11efe928b16149a906aef3643c3d10af5
rayves <vo.raymond.v@outlook.com>
2022-07-28 21:21:30 +1000

    refactors json reading into a method | adds go income

--- src/game.rb
+++ src/game.rb
@@ -1,9 +1,11 @@
 require 'json'
 
 # Import external data and convert them into usable data for the game
-BOARD = JSON.parse(File.read('data/board.json'))
-ROLLS_ONE = JSON.parse(File.read('data/rolls_1.json'))
-ROLLS_TWO = JSON.parse(File.read('data/rolls_2.json'))
+BOARD = read_json('data/board.json')
+ROLLS_ONE = read_json('data/rolls_1.json')
+ROLLS_TWO = read_json('data/rolls_2.json')
 
 # Order the names within the PLAYER_NAMES array determines the order of rolls for the players.
 PLAYER_NAMES = %w[Peter Billy Charlotte Sweedal].freeze
+
+GO_INCOME = 1
